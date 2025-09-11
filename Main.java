60412a355633cbe4f1d2547ac9d20e5063e2d6f1
harithh0 <169714068+harithh0@users.noreply.github.com>
2025-09-10 22:02:47 -0500

    first

---
+++ Main.java
@@ -0,0 +1,7 @@
+import java.sql.*;
+
+public class Main {
+  private static final String DB_URL = "jdbc:sqlite:test.db";
+
+  public static void main(String[] args) {}
+}
---
+++ src/PasswordEntry.java
@@ -0,0 +1,14 @@
+public class PasswordEntry {
+
+  private String username;
+  private String password;
+  private String site;
+  private String notes;
+
+  PasswordEntry(String username, String password, String site, String notes) {
+    this.username = username;
+    this.password = password;
+    this.site = site;
+    this.notes = notes;
+  }
+}
